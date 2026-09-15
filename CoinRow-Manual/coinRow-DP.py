5b400b21b0365842981924ef6d78fb0369ca9650
Hizu <bandorifordawin20@gmail.com>
2026-09-16 06:00:31 +0800

    feat: seperated different solutions and added a readme file which doubles as report.

---
+++ CoinRow-Manual/coinRow-DP.py
@@ -0,0 +1,78 @@
+#Didn't put any error handling to make it less intimidating :)
+#function initialization here btw
+
+def dynamicProgramming(coins):
+    n = len(coins) #n = number of elements in the list
+
+    #refers to the F(1) = C[1]
+    #If there's only one coin, then taking it is negligible
+    if n == 1:
+        return coins[0], [coins[0]]
+
+    #-- Step 1 - DP Table Initialization --
+    #dp[i] represents the maximum value possible by only looking at coins[0...i]
+    dp = [0] * n
+
+
+    #-- Step 2 - Base Cases Definition --
+    #For coin[0], you only have one coin available so it will be the maximum value
+    dp[0] = coins[0]
+
+
+    #For coin[1], you can take coin[0] OR coin[1] but never both. Therefore pick the larger number.
+    dp[1] = max(coins[0], coins[1]) #max() basically takes the largest value out of the parameters included.
+
+
+    #-- Step 3 - Filling the Table (Recurrence Relation) --
+    # In every coin[i], test the two choices.
+    for i in range(2 , n):
+        #Option 1: Take coin[i]
+        #However you won't be able to pick coin[i-1]
+        #So what we'll do is add the best score from 2 spots back (dp[i-2])
+        take_coin = coins[i] + dp[i - 2]
+
+        #Option 2: Skip coin[i]
+        #Gain 0 from coin i, best score will be carried over from dp[i-1]
+        skip_coin = dp[i-1]
+
+        #Store on what coin has the higher value
+        dp[i] = max(take_coin, skip_coin)
+
+    #-- [OPTIONAL] Step 4: Backtrack (to find out what coins won) --
+    #Tracing backwards to see what choices the program made.
+
+    chosen_coins = []
+    i = n-1
+
+    while i>= 0:
+        if i == 0:
+            #If the first coin was taken
+            chosen_coins.append(coins[0])
+        elif i == 1:
+            #reached the second coin and check if which coin won
+            if dp[1] == coins[1]:
+                chosen_coins.append(coins[1])
+            else:
+                chosen_coins.append(coins[0])
+            break
+        elif dp[i] == coins[i] + dp[i - 2]:
+            #if dp[i] matches the take_coin's formula then it is selected
+            chosen_coins.append(coins[i])
+            i -= 2 #jump to skip the adjacent element
+        else:
+            #dp[i] == dp[i-1], coin[i] was skipped.
+            i -=1
+
+    chosen_coins.reverse()
+
+    return dp[-1], chosen_coins
+
+
+#Function in action
+
+coin_list = [5,1,9,10,9,2]
+
+max_total, selected = dynamicProgramming(coin_list)
+print(f"Coin Row:       {coin_list}")
+print(f"Max Value:      Php {max_total}")
+print(f"Selected Coins: {selected}")
---
+++ README.md
@@ -0,0 +1,68 @@
+<h1 style="text-align: center;">Data Structures and Algorithm - Coin Row Problem</h1>
+<h5 style="text-align: center;">Algorithm Analysis for the Coin Row Problem</p>
+
+<h6 style="text-align: center;"><a href="Google colab notebook link here" > [Cloud version] Google Colab Notebook</a></h6>
+
+---
+
+## Problem
+
+Given that a row of N coins with **positive values**:
+
+<p style="text-align: center;"> $C_1 , C_2, C_3, ..., C_n$</p>
+
+The goal we need to achieve is to ensure that:
+<ul>
+    <li>Selected coins <b>HAVE</b> the <b><i>MAXIMUM TOTAL VALUE</i></b></li>
+    <li><b>NO</b> two selected coins are <i>adjacent</i></li>
+</ul>
+
+
+Throughout this repository (or report, take it as you will lol) we'll be messing with this set of numbers using a list:
+
+<p style="text-align: center;"> $[ 5 , 1 , 9 , 10 , 9 , 2]$</p>
+
+This repo will also only utilize Python for the programming and analysis of the Space-Time Complexity.
+
+> If you want a cool challenge, you can always try shooting your shot in translating the code to other languages :3, do not hesitate to shoot a message if you want further clarification or if you dont understand bits of the concept.
+
+---
+## Solution
+
+Honestly there are probably multiple algorithms I didn't know that you can use to solve this problem. However, I'll only be focusing on 3 strategies.
+
+Always keep in mind that there are no "better" or all-size-fits-all algorithm, it always depends on the problem and your approach.
+
+In this case, the 3 strategies we will use are:
+
+<!-- no toc -->
+  - [Greedy Approach](#greedy-approach)
+  - [Brute Force Approach](#brute-force-approach)
+  - [Dynamic Programming](#dynamic-programming)
+
+This repo will go in-depth with each approach including the computation for the Space-Time Complexity then put these three against each other and help you understand why <b>Dynamic Programming Approach</b> is the most optimal strategy out them.
+
+---
+### Greedy Approach
+##### [Click me for the code](./CoinRow-Manual/coinRow-GS.py)
+
+Greedy Approach or Greedy Algorithm is where we will automatically collects the largest non-adjacent numbers in the list and claim it as the maximum value.
+
+By itself, is probably what people would use to get the maximum value of a list. Unfortunately, this algorithm is very short-sighted.
+
+To see what I mean, you can run the code in this repo o
+
+---
+### Brute Force Approach
+
+
+---
+### Dynamic Programming
+
+
+---
+### Greedy vs Brute Force vs Dynamic
+
+
+---
+### Conclusion
